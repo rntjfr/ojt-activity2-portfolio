1dc886ab45f0302c11271c1530545e0964294a74
Rex Tautho <127009586+rntjfr@users.noreply.github.com>
2024-08-29 15:46:08 +0800

    color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Certifications from './components/Certifications';
-
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Certifications from "./components/Certifications";
 
 function App() {
   return (
-    <div className="bg-gradient-to-br from-slate-900 via-gray-950 to-zinc-950 min-h-screen text-white">
+    <div className="bg-gray-950 min-h-screen text-white">
       <Header />
       <Hero />
       <About />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export default function Hero() {
-    return (
-        <section className="p-20 h-screen flex flex-col items-center">
+  return (
+    <section className="p-20 h-screen flex flex-col items-center">
+      <div className="py-2 -ml-60 relative z-10 text-xl font-bold text-left whitespace-nowrap">
+        <h2>Rexdan Tautho</h2>
+      </div>
 
-            <div className="py-2 -ml-60 relative z-10 text-xl font-bold text-left whitespace-nowrap"><h2>Rexdan Tautho</h2></div>
+      <div className="flex relative">
+        <div className="w-110 h-170 border-4 border-white bg-transparent relative z-0">
+          <div className="w-60 h-120 bg-white"></div>
+        </div>
 
-            <div className="flex relative">
-                <div className="w-110 h-170 border-4 border-white bg-transparent relative z-0">
-                    <div className="w-60 h-120 bg-white"></div>
-                </div>
-
-                <div className="py-20 px-20 -ml-90 relative z-10">
-                    <h1 className="text-7xl font-aileron text-white font-bold">Career</h1>
-                    <h1 className="text-7xl font-aileron text-white font-bold">Portfolio.</h1>
-                    <h2 className="text-2xl font-bold text-left whitespace-nowrap">Information Technology Specialist</h2>
-                    <h2 className="text-5xl font-bold text-left whitespace-nowrap px-20 -mt-9">______________</h2>
-
-                </div>
-            </div>
-            <div className="py-2 -ml-80 relative z-10 text-xl font-bold text-left whitespace-nowrap"><h3>2020 - 2024</h3></div>
-
-
-
-        </section>
-    );
+        <div className="py-20 px-20 -ml-90 relative z-10">
+          <div className="bg-gray-950 p-3">
+            <h1 className="text-7xl font-aileron text-white font-bold">
+              Career
+            </h1>
+            <h1 className="text-7xl font-aileron text-white font-bold">
+              Portfolio.
+            </h1>
+            <h2 className="text-2xl font-bold text-left whitespace-nowrap">
+              Information Technology Specialist
+            </h2>
+            <h2 className="text-5xl font-bold text-left whitespace-nowrap px-20 -mt-9">
+              ______________
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="py-2 -ml-80 relative z-10 text-xl font-bold text-left whitespace-nowrap">
+        <h3>2020 - 2024</h3>
+      </div>
+    </section>
+  );
 }
